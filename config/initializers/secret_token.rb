61a37d3d7e680293e9dd1ded281260487dc1125a
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-09-15 16:46:53 +0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stubbles::Application.config.secret_token = '06e6071d758be5e020d7c6e602662501300a985c556dc5703d91436bccfc1ac41aa173fe96a8be6b6eab67f081beac4d9f479fbee53a66c193227182d2131edc'
