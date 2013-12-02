5506656a1bddf67c16b022c286534d7c0237d257
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-12-02 11:43:50 +0600

    Using hogan instead of mustache

--- app/assets/javascripts/template.js
+++ app/assets/javascripts/template.js
@@ -1,10 +1,7 @@
-//= require mustache
-
 var Template = {
     render: function (template, values) {
-//        var compiledTemplate = Hogan.compile(template);
-//        return compiledTemplate.render(values);
-
-        return Mustache.render(template, values);
+        var compiledTemplate = Hogan.compile(template);
+        return compiledTemplate.render(values);
+//        return Mustache.render(template, values);
     }
 }
