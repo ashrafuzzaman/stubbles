466a0559ca2ac015870ba0af2814a41637416e6b
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-12-04 11:02:02 +0600

    Added sendgrid mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Stubbles::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -65,4 +65,14 @@ Stubbles::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  ActionMailer::Base.smtp_settings = {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :authentication => :plain,
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 end
