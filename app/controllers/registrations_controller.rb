f5ed41b4e6a9d06dec758f8920c1375ad0cb617f
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-01-18 15:09:54 +0600

    Added captcha

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
-    if verify_recaptcha
-      super
+    if Rails.env.production?
+      if verify_recaptcha
+        super
+      else
+        build_resource(sign_up_params)
+        clean_up_passwords(resource)
+        flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
+        flash.delete :recaptcha_error
+        render :new
+      end
     else
-      build_resource(sign_up_params)
-      clean_up_passwords(resource)
-      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-      flash.delete :recaptcha_error
-      render :new
+      super
     end
   end
 end
