90f18122d8c836b736cee3845db9568d822a172c
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-01-20 19:02:01 +0600

    Working on work flow

--- app/models/workflow_status.rb
+++ app/models/workflow_status.rb
@@ -2,4 +2,5 @@ class WorkflowStatus < ActiveRecord::Base
   belongs_to :project
 
   validates :title, :workflow_for, :project_id, presence: true
+  attr_accessible :title, :description, :workflow_for
 end
