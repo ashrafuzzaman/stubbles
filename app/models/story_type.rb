283c2ee057dc561d2e44285979d0f8fe88bde3db
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-01-22 19:11:37 +0600

    Added transition

--- app/models/story_type.rb
+++ app/models/story_type.rb
@@ -1,6 +1,7 @@
 class StoryType < ActiveRecord::Base
   belongs_to :project
   has_many :stories
+  has_many :workflow_transitions
 
   validates :title, :project_id, presence: true
   attr_accessible :title, :description
