19e2c1abf8e8ff03eff98a017f219a527defe55e
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-01-24 17:54:07 +0600

    Added validation to have only one initial state

--- app/models/workflow_status.rb
+++ app/models/workflow_status.rb
@@ -2,6 +2,17 @@ class WorkflowStatus < ActiveRecord::Base
   belongs_to :project
   belongs_to :workflowable, polymorphic: true
 
-  validates :title, :project_id, presence: true
-  attr_accessible :title, :description, :workflowable_type, :workflowable_id
+  validates :title, presence: true
+  validate :at_least_one_initial_state
+  attr_accessible :title, :description, :workflowable_type, :workflowable_id, :initial_status
+
+  private
+  def at_least_one_initial_state
+    init_workflow_count = workflowable.workflow_statuses.where(initial_status: true).count
+    ap init_workflow_count
+    init_workflow_count += 1 if (new_record? or initial_status_was == false) and initial_status?
+    init_workflow_count -= 1 if persisted? and initial_status_was == true and initial_status == false
+
+    errors.add(:initial_status, "There should only one initial status") if init_workflow_count > 1
+  end
 end
--- app/models/workflow_transition.rb
+++ app/models/workflow_transition.rb
@@ -4,6 +4,7 @@ class WorkflowTransition < ActiveRecord::Base
   belongs_to :to_status, class_name: 'WorkflowStatus'
 
   validates :event, :from_status_id, :to_status_id, presence: true
+  validates :to_status_id, uniqueness: {scope: :from_status_id}
 
   attr_accessible :event, :from_status_id, :to_status_id
 end
