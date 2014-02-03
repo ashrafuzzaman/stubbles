31468c57b128f796ad6e0a3a3d994da82b1c2d1c
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-02-03 19:32:53 +0600

    Working with story status sync

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -2,16 +2,26 @@ require 'spec_helper'
 
 describe Story do
   describe '#propagate_values_to_story' do
-    context 'with new task' do
+    context 'with no task' do
       before do
-        @story = FactoryGirl.create(:story, milestone: FactoryGirl.create(:milestone), story_type: StoryType.where(title: 'Feature').first)
-        #FactoryGirl.create(:task, story: story, hours_estimated: 2, hours_spent: 2, percent_completed: 100)
+        project = FactoryGirl.create(:project)
+        @story = FactoryGirl.create(:story, project: project, milestone: FactoryGirl.create(:milestone), story_type: StoryType.where(title: 'Feature').first)
+      end
+      subject { @story }
+      its(:workflow_status) { should be nil }
+    end
+
+    context 'with 2 tasks' do
+      before do
+        project = FactoryGirl.create(:project)
+        st = StoryType.where(title: 'Feature').first
+        @story = FactoryGirl.create(:story, project: project, milestone: FactoryGirl.create(:milestone), story_type: st)
+        FactoryGirl.create(:task, story: story, hours_estimated: 2, workflow_status: st.workflow_statuses.where(title: 'Open'))
+        FactoryGirl.create(:task, story: story, hours_estimated: 2, workflow_status: st.workflow_statuses.where(title: 'Qa approved'))
       end
 
-      it 'should propagate to story and mile stone' do
-        ap @story.story_type.workflow_statuses.all
-        expect(@story.workflow_status.title).to eq 'Open'
-      end
+      subject { @story.reload }
+      its(:workflow_status) { should be nil }
     end
   end
 end
