faf8daf1368f9199cb86e732598e8fa15cc692b9
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-02-06 00:29:17 +0600

    Changed task view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,11 @@ class TasksController < InheritedResources::Base
   belongs_to :story
   respond_to :html, :xml, :json, :js
 
+  def new
+    new!
+    @task.workflow_status = @task.story.story_type.initial_workflow_status
+  end
+
   def update_status
     resource.update_attributes workflow_status_id: params[:workflow_status_id]
   end
