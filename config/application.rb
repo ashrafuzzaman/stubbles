f28f1c613bd339643e206e16141b55f390df060d
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-03-23 15:40:43 +0600

    Changed the precompile list

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Stubbles
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += %w(dashboard.js sign_in.css calendar.js dark_theme light_theme)
+    config.assets.precompile += %w(dashboard.js sign_in.css calendar.js custom_bootstrap/dark_theme.css custom_bootstrap/light_theme.css)
 
     config.generators.stylesheets = false
     config.generators.javascripts = false
