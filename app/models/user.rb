a39b7cccb0e9b7edf14e9e269c7dbca177a37a0b
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-03-28 20:05:19 +0600

    Working with google callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,14 +43,20 @@ class User < ActiveRecord::Base
     "http://www.gravatar.com/avatar/#{email_hash}?s=#{size}"
   end
 
+  def password_required?
+    #super && provider.blank?
+    false
+  end
+
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:email => data["email"]).first
 
     unless user
+      ap data
       user = User.create(name: data["name"],
-                         email: data["email"],
-                         password: Devise.friendly_token[0, 20]
+                         email: data["email"]
+      #password: Devise.friendly_token[0, 20]
       )
     end
     user
