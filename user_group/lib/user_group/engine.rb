f8db01bd897fde5c6e5f17742e99beda81cd9abf
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-04-01 14:22:08 +0600

    Updated generators

--- user_group/lib/user_group/engine.rb
+++ user_group/lib/user_group/engine.rb
@@ -1,4 +1,14 @@
 module UserGroup
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :erb
+      g.test_framework :rspec
+      g.integration_tool :rspec
+      g.fixture_replacement :factory_girl
+      g.stylesheets = false
+      g.javascripts = false
+      g.scaffold_controller = :scaffold_controller
+    end
   end
 end
